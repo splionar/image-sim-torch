43f192a584840dc7683418ceb37e52bbf4cb72a8
Stefan Putra Lionar <31429575+splionar@users.noreply.github.com>
2020-05-20 00:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ for epoch in range(1, n_epochs+1):
         
         it = it + 1
         
-        if it%1 == 0:
+        if it%10 == 0:
             print("Iteration: {} Loss: {}".format(it,100*loss))
             
     # print avg training statistics 
