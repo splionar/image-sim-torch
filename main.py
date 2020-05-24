e3238d91c327be70dd63033415ae222c9db6e951
Stefan Putra Lionar <31429575+splionar@users.noreply.github.com>
2020-05-24 14:56:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ class TripletAlexNet(nn.Module):
         self.avgpool = nn.AdaptiveMaxPool2d((8, 8))
         self.fcn = nn.Sequential(
             nn.Dropout(p=0.4),
-            nn.Linear(int(256/div) * 8 * 8, int(4096/8))
+            nn.Linear(int(256/div) * 8 * 8, int(4096/8)),
             nn.ReLU(),
             nn.Dropout(p = 0.4),
             nn.Linear(int(4096/8), int(4096/8))
