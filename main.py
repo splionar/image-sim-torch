707ede3f233b90e62ec3c92e7abebe0eb27634a7
Stefan Putra Lionar <31429575+splionar@users.noreply.github.com>
2020-06-05 03:07:44 +0200

    weight decay

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ model = UNet()
 model.cuda()
 
 # specify loss function
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0005)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0005, weight_decay=0.001)
 
 # number of epochs to train the model
 n_epochs = 10000
@@ -414,7 +414,7 @@ for epoch in range(1, n_epochs+1):
 
         if it%1000 == 0:
             #print('Saving model')
-            torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/out_unet2/unet2.pt")
+            torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/out_unet2/unet2_reg01.pt")
           
     # print avg training statistics 
     train_loss = train_loss/len(train_loader)
@@ -426,6 +426,6 @@ for epoch in range(1, n_epochs+1):
     print('Saving model')
     
     if ep%5 == 0:
-        torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/out_unet2/unet2_epoch{}.pt".format(ep))
+        torch.save(model.state_dict(), "/content/drive/My Drive/IML/task4/out_unet2/unet2__reg01_epoch{}.pt".format(ep))
     
     ep = ep + 1
